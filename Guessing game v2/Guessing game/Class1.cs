6ae7b3615e57a64b90bd44a6020e874f6e266c3c
AlexandrGambarov <94642539+AlexandrGambarov@users.noreply.github.com>
2022-09-26 21:32:01 +0300

    Add files via upload

---
+++ Guessing game v2/Guessing game/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guessing_game
+{
+    class Class1
+    {
+        public static void GuessingGame(int guess)
+        {
+            Random random = new Random();
+
+            int min = 1;
+            int max = 3;
+            int number = random.Next(min, max + 1);
+
+            Console.WriteLine("Угадай число от " + min + " до " + max + " : ");
+            Console.WriteLine("Твое число: " + guess);
+
+            if (guess > number)
+            {
+                Console.WriteLine(guess + " слишком большое.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine(guess + " слишком маленькое.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            GuessingGame(3);
+        }
+    }
+}
